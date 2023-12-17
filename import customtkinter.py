cb2d81164e0ccba6f0ee819661d277abedf85afb
Michael <152120453+Michael18hdw05@users.noreply.github.com>
2023-12-17 13:35:32 -0300

    Add files via upload

---
+++ import customtkinter.py
@@ -0,0 +1,27 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+janela = customtkinter.CTk()
+janela.geometry("500x300")
+
+def clique():
+    print("Fazer Loguin")
+
+texto = customtkinter.CTkLabel(janela, text="Fazer Login")
+
+email = customtkinter.CTkEntry(janela,
+                               placeholder_text="E-mail")
+senha = customtkinter.CTkEntry(janela,
+                               placeholder_text="Senha",
+                               show="*")
+
+botao = customtkinter.CTkButton(janela, text="Loguin",
+                                command=clique)
+
+texto.pack(padx=10, pady=10)
+email.pack(padx=10, pady=10)
+senha.pack(padx=10, pady=10)
+botao.pack(padx=10, pady=10)
+janela.mainloop()
